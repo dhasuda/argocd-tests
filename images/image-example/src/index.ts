5f64db8830e94942d26c71642a2db1a47066039f
dhasuda <davihasuda@gmail.com>
2021-01-22 16:18:12 -0300

    Testing changes

--- images/image-example/src/index.ts
+++ images/image-example/src/index.ts
@@ -2,14 +2,12 @@ import express from 'express'
 
 export const PORT = 8080
 
-// Testing
-
 const app = express()
 app.use(express.json())
 
 app.post('/', async (_, res) => {
   res.statusCode = 200
-  res.send('Hello, world!')
+  res.send('Hello, world! This is a test')
 })
 
 app.listen(PORT)
